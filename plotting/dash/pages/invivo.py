c2dfc4c5ef899a9ec6d693ac04c0903cb015b2ed
Gruenberger <gruenbergeraw@nih.gov>
2023-06-13 19:27:11 -0400

    updated invivo
    handles routes better. Handles some other situations more easily.

--- plotting/dash/pages/invivo.py
+++ plotting/dash/pages/invivo.py
@@ -2,6 +2,9 @@
 Invivo experiments are selected and data plots are displayed.
 
 As of 6/2/2023 this section needs to be updated due to a new data model, and additional approaches on plots.
+As of 6/13/2023 this section has been reworked a bit, but the case of looking at individual animals
+    per group might not be useful-- or needs to be addressed so as not to interfere with navigation from
+    compounds list link.
 """
 import dash
 from dash import html, dcc, Input, Output, State, ctx
@@ -9,38 +12,52 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 from .dataservice import dataService
 
-dash.register_page(__name__, path='/invivo')
+dash.register_page(__name__, path='/invivo', path_template='/invivo/<expid>/<nsc>')
 
 ROW_PADDING = {
     "paddingTop": "calc(var(--bs-gutter-x) * .5)",
     "paddingBottom": "calc(var(--bs-gutter-x) * .5)"
 }
 
-# The left select for menus associated with Invivo experiments
-left_select = dbc.Card(body=True, children=[
-    html.Div(id='input-form-div', children=[
-        dbc.Form(id='input-form', children=[
-            dbc.Row(children=[
-                html.Div(className='d-grid', children=[
-                    dbc.Label("Select Experiment Number", html_for="s-expid-dropdown"),
-                    dcc.Dropdown(id="s-expid-dropdown", placeholder="Select")
-                ])
-            ], style=ROW_PADDING),
-            dbc.Row(children=[
-                html.Div(className='d-grid', children=[
-                    dbc.Label("Select NSC Number", html_for="s-nsc-dropdown"),
-                    dcc.Dropdown(id="s-nsc-dropdown", disabled=True)
-                ])
-            ], style=ROW_PADDING),
-            dbc.Row(children=[
-                html.Div(className='d-grid', children=[
-                    dbc.Label("Select Group Number", html_for="s-group-dropdown"),
-                    dbc.Select(id="s-group-dropdown", disabled=True)
-                ])
-            ], style=ROW_PADDING)
+
+def get_left_select(expid, nsc):
+    """
+    The left select for menus associated with invivo experiments. Modified to handle dynamic URLs
+    :param expid: string: the expid for given experiment
+    :param nsc: string: NSC selected from compounds list
+    :return: dcc.Component: html.Div that contains the various components of the left-select section
+    """
+    if expid is None and nsc is None:
+        expid_dropdown = dcc.Dropdown(id="s-expid-dropdown", placeholder="Select")
+        nsc_dropdown = dcc.Dropdown(id="s-nsc-dropdown", disabled=True)
+    else:
+        expid_dropdown = dcc.Dropdown(id="s-expid-dropdown", value=expid, options=[{'label': expid, 'value': expid}])
+        nsc_dropdown = dcc.Dropdown(id="s-nsc-dropdown", value=nsc, options=[{'label': nsc, 'value': nsc}])
+
+    return dbc.Card(body=True, children=[
+        html.Div(id='input-form-div', children=[
+            dbc.Form(id='input-form', children=[
+                dbc.Row(children=[
+                    html.Div(className='d-grid', children=[
+                        dbc.Label("Select Experiment Number", html_for="s-expid-dropdown"),
+                        expid_dropdown
+                    ])
+                ], style=ROW_PADDING),
+                dbc.Row(children=[
+                    html.Div(className='d-grid', children=[
+                        dbc.Label("Select NSC Number", html_for="s-nsc-dropdown"),
+                        nsc_dropdown
+                    ])
+                ], style=ROW_PADDING),
+                dbc.Row(children=[
+                    html.Div(className='d-grid', children=[
+                        dbc.Label("Select Group Number", html_for="s-group-dropdown"),
+                        dbc.Select(id="s-group-dropdown", disabled=True)
+                    ])
+                ], style=ROW_PADDING)
+            ])
         ])
     ])
-])
 
 
 @dash.callback(
@@ -80,16 +97,34 @@ def handle_selections(expid, nsc, data):
                 group number dropdown
     """
     changed = ctx.triggered_id
+    nscs = str()
+    if expid is not None:
+        res = [d for d in dataService.INVIVO_COLL.aggregate(
+            [
+                {
+                    '$match': {
+                        'expid': expid
+                    }
+                }, {
+                    '$project': {
+                        'invivonsc': 1,
+                        '_id': 0
+                    }
+                }
+            ]
+        )]
+        #[ "'string','string'", "'string','string'"]
 
+        for doc in res:
+            nscs = nscs + doc['invivonsc']
+        nscs = nscs.replace('\'', '').split(',')
+        nscs = [int(x) for x in nscs]
     # Checks to see which Input was changed using the 'changed' context trigger ID, and handles request
     if (changed == 's-expid-dropdown') and (expid is not None):
-        nscs = data['invivo_dict'][expid]
-
         # this is a best first attempt at handling NSCs that are actually control values. Passes NSC list back
         return [{"label": "Control", "value": x} if (x == 999999) else {"label": x, "value": x} for x in
                 nscs], False, [], True
     elif (changed == 's-nsc-dropdown') and (expid is not None):
-        nscs = data['invivo_dict'][expid]
         group_nbrs = dataService.get_invivo_group_numbers(nsc, expid)
         group_labels = [{"label": "1 - Control", "value": x} if (x == 0) else {"label": x + 1, "value": x} for x in
                         group_nbrs]
@@ -124,24 +159,7 @@ def get_graphs(active_tab, group, nsc, expid):
         # The summary tab displays similarly to the supplier report for invivo with box plots for each group
         # and a table describing the treatment and composition of the groups.
         if active_tab == 'summ-tab':
-            data = dataService.get_invivo_summary_plots(expid)
-            desc = data['descriptions']
-            rows = [
-                dbc.Row(dbc.Col(html.H5(f'Summary of Invivo NSC {nsc} | Expid {data["expid"]}'))),
-                dbc.Row([
-                    dbc.Col(dcc.Graph(figure=data['net_wt_fig']), width=6),
-                    dbc.Col(dcc.Graph(figure=data['tum_wt_fig']), width=6)
-                ]),
-                dbc.Row(dbc.Col(html.P(f'Implant Date: {data["implant_dt"]}, Staging Date: {data["staging_dt"]}'))),
-                dbc.Row(
-                    dbc.Col(dbc.Table([html.Thead(html.Tr([html.Th('Groups'), html.Th('Description')]))] +
-                                      [html.Tbody(
-                                          [html.Tr([html.Td(x['group']), html.Td(x['description'])]) for x in desc])
-                                      ]
-                                      , bordered=True))
-                )
-            ]
-            return rows
+            return get_summary_components(expid, nsc)
         # This creates a Kaplan-Meier curve for a given group along with the control group
         if active_tab == 'survival-tab':
             return dcc.Graph(figure=dataService.get_km_graph(expid, nsc, group))
@@ -167,24 +185,61 @@ def get_graphs(active_tab, group, nsc, expid):
         return html.P('Please select Exp Nbr, NSC, and Group')
 
 
-# Layout object that is the main wrapper of this whole module
-layout = dbc.Row(children=[
-    dbc.Col(left_select, width=2),
-    dbc.Col(dbc.Card([
-        dbc.CardHeader(
-            dbc.Tabs(
-                [
-                    dbc.Tab(tab_id='summ-tab', label="Summary"),
-                    dbc.Tab(tab_id='survival-tab', label="Survival"),
-                    dbc.Tab(tab_id='average-tab', label="Averages"),
-                    dbc.Tab(tab_id='boxes-tab', label="Boxes")
-                ],
-                active_tab='summ-tab',
-                id='s-graph-tabs',
-            )),
-        dbc.CardBody(dcc.Loading(id='s-graph-content-loading', type='default',
-                                 children=html.Div(html.P('Please select Exp Nbr, NSC, and Group'),
-                                                   id='s-graph-content')))
-    ]), width=10
-    )
-], style=ROW_PADDING)
+def get_summary_components(expid, nsc):
+    """
+    Function to generate the components to replicate a supplier reports invivo page. Made into separate function for
+    clarity.
+    :param expid: string: The experiment ID
+    :param nsc: string: the NSC
+    :return: list: dash components containing the graphs and table for look and feel of summary report
+    """
+    data = dataService.get_invivo_summary_plots(expid)
+    desc = data['descriptions']
+    rows = [
+        dbc.Row(dbc.Col(html.H5(f'Summary of Invivo NSC {nsc} | Expid {data["expid"]}'))),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=data['net_wt_fig']), width=6),
+            dbc.Col(dcc.Graph(figure=data['tum_wt_fig']), width=6)
+        ]),
+        dbc.Row(dbc.Col(html.P(f'Implant Date: {data["implant_dt"]}, Staging Date: {data["staging_dt"]}'))),
+        dbc.Row(
+            dbc.Col(dbc.Table([html.Thead(html.Tr([html.Th('Groups'), html.Th('Description')]))] +
+                              [html.Tbody(
+                                  [html.Tr([html.Td(x['group']), html.Td(x['description'])]) for x in desc])
+                              ]
+                              , bordered=True))
+        )
+    ]
+    return rows
+
+
+def layout(expid=None, nsc=None):
+    """
+    Layout object that is the main wrapper of this whole module. Function to handle dynamic URL patterns
+    :param expid: string: the experiment ID
+    :param nsc: string: the NSC selected from compounds list
+    :return: dbc.Row: a dash bootstrap component Row containing all the section content.
+    """
+    if expid is None and nsc is None:
+        card_body = html.Div(html.P('Please select Exp Nbr, NSC, and Group'), id='s-graph-content')
+    else:
+        card_body = html.Div(get_summary_components(expid, nsc))
+    return dbc.Row(children=[
+        dbc.Col(get_left_select(expid, nsc), width=2),
+        dbc.Col(dbc.Card([
+            dbc.CardHeader(
+                dbc.Tabs(
+                    [
+                        dbc.Tab(tab_id='summ-tab', label="Summary"),
+                        dbc.Tab(tab_id='survival-tab', label="Survival"),
+                        dbc.Tab(tab_id='average-tab', label="Averages"),
+                        dbc.Tab(tab_id='boxes-tab', label="Boxes")
+                    ],
+                    active_tab='summ-tab',
+                    id='s-graph-tabs',
+                )),
+            dbc.CardBody(dcc.Loading(id='s-graph-content-loading', type='default',
+                                     children=card_body))
+        ]), width=10
+        )
+    ], style=ROW_PADDING)
